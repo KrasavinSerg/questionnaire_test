09bfa634b58530bc42a0bf9fb9ed4eb1b736fb42
KrasavinSerg <krasavin.serg@gmail.com>
2018-10-23 14:06:50 +0300

    added js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$('.nav__toggle').click(function() {
 		$('.nav').toggleClass('nav--opened');
 	});
+
+	$('input[type="range"]').rangeslider({
+		polyfill: false
+	});
 });
